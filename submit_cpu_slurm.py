6d30258f996afb7b70355b56b5680baedbc7a00f
andrea-w-wang <ayw255@nyu.edu>
2023-05-08 13:01:37 -0400

    change script to take start_month and end_month

--- submit_cpu_slurm.py
+++ submit_cpu_slurm.py
@@ -1,13 +1,14 @@
 import argparse
 import os
 import pathlib
+
 current_file_location = str(pathlib.Path(__file__).parent.resolve())
 
 PROJECT_FOLDER = current_file_location.split("/")[-1]
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-py", "--py_func", required=True, type=str)
-parser.add_argument("-y", "--years", required=True, type=str, nargs="+")
+parser.add_argument("-m", "--months", required=True, type=str, nargs="2")
 parser.add_argument("-v", "--variable", type=str)
 args = parser.parse_args()
 variable_name = None
@@ -18,33 +19,33 @@ if args.variable:
 
 job_prefix = args.py_func.split(".")[0]
 # write sh file
-for year in args.years:
-    filename=f"{job_prefix}-{year}"
-    with open(f"./{filename}.sh", "w") as f:
-        if not variable_value:
-            f.write(
-                f"""#!/bin/bash
-    #echo "Activating huggingface environment"
-    #source /share/apps/anaconda3/2021.05/bin/activate huggingface
-    echo "Beginning script"
-    cd /share/luxlab/andrea/{PROJECT_FOLDER}
-    python3 {args.py_func} --t C --start-year {year} --end-year {year}
-                """
-            )
-        else:
-            f.write(
-                f"""#!/bin/bash
-                #echo "Activating huggingface environment"
-                #source /share/apps/anaconda3/2021.05/bin/activate huggingface
-                echo "Beginning script"
-    		cd /share/luxlab/andrea/{PROJECT_FOLDER}
-    		python3 {args.py_func} --t C --start-year {year} --end-year {year} --{variable_name} {variable_value}
-                            """
-        )
-
-    with open(f"./{filename}.sub", "w") as f:
+start_month, end_month = args.months
+filename = f"{job_prefix}-{start_month}-{end_month}"
+with open(f"./{filename}.sh", "w") as f:
+    if not variable_value:
         f.write(
             f"""#!/bin/bash
+#echo "Activating huggingface environment"
+#source /share/apps/anaconda3/2021.05/bin/activate huggingface
+echo "Beginning script"
+cd /share/luxlab/andrea/{PROJECT_FOLDER}
+python3 {args.py_func} --t C --start-month {start_month} --end-month {end_month}
+            """
+        )
+    else:
+        f.write(
+            f"""#!/bin/bash
+            #echo "Activating huggingface environment"
+            #source /share/apps/anaconda3/2021.05/bin/activate huggingface
+            echo "Beginning script"
+        cd /share/luxlab/andrea/{PROJECT_FOLDER}
+        python3 {args.py_func} --t C --start-month {start_month} --end-month {end_month} --{variable_name} {variable_value}
+                        """
+        )
+
+with open(f"./{filename}.sub", "w") as f:
+    f.write(
+        f"""#!/bin/bash
 #SBATCH -J {filename}                            # Job name
 #SBATCH -o /share/luxlab/andrea/{PROJECT_FOLDER}logs/{filename}_%j.out # output file (%j expands to jobID)
 #SBATCH -e /share/luxlab/andrea/{PROJECT_FOLDER}logs/{filename}_%j.err # error log file (%j expands to jobID)
@@ -57,7 +58,7 @@ for year in args.years:
 #SBATCH -t 20:00:00                            # Time limit (hh:mm:ss)
 #SBATCH --partition=default_partition          # Request partition
 /share/luxlab/andrea/{PROJECT_FOLDER}/{filename}.sh
-            """
-        )
-    os.system(f"chmod 775 {filename}.sh")
-    os.system(f"sbatch --requeue {filename}.sub")
+        """
+    )
+os.system(f"chmod 775 {filename}.sh")
+os.system(f"sbatch --requeue {filename}.sub")
